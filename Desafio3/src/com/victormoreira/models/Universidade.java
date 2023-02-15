1a82e459a8c44682537c4ee5b4010d965201fd5c
victormoreira92 <victormoreira.ti@outlook.com>
2023-02-15 15:47:13 -0300

    add gets e sets na classe

--- Desafio3/src/com/victormoreira/models/Universidade.java
+++ Desafio3/src/com/victormoreira/models/Universidade.java
@@ -1,7 +1,7 @@
 package com.victormoreira.models;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
+
+import java.util.List;
 
 public class Universidade {
 	private List<Aluno> alunosMatriculados;
@@ -11,6 +11,21 @@ public class Universidade {
 		
 	}
 
-	
+	public List<Aluno> getAlunosMatriculados() {
+		return alunosMatriculados;
 	}
+
+	public void setAlunosMatriculados(List<Aluno> alunosMatriculados) {
+		this.alunosMatriculados = alunosMatriculados;
+	}
+
+	public List<Curso> getCursosOferecidos() {
+		return cursosOferecidos;
+	}
+
+	public void setCursosOferecidos(List<Curso> cursosOferecidos) {
+		this.cursosOferecidos = cursosOferecidos;
+	}
+
+	
 }
