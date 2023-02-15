83f85c41141f71759ba6cb74fa7cddf3a09e4d05
victormoreira92 <victormoreira.ti@outlook.com>
2023-02-15 15:26:28 -0300

    refatorar classe Universidade tirando classe utilitaria

--- Desafio3/src/com/victormoreira/models/Universidade.java
+++ Desafio3/src/com/victormoreira/models/Universidade.java
@@ -6,22 +6,11 @@ import java.io.FileReader;
 public class Universidade {
 	private List<Aluno> alunosMatriculados;
 	private List<Curso> cursosOferecidos;
-	private String path = "arquivoUniversidade.csv";
 	
-	public Universidade() throws Exception{
+	public Universidade() {
+		
 	}
 
-	private void criarDadosUniversidade() throws Exception {
-		try(BufferedReader br = new BufferedReader(new FileReader(path))){
-			String line = br.readLine();
-			int dados = 0;
-			while(line != null) {
-				dados++;
-				line = br.readLine();
-			}
-			System.out.println("Foram lidas "+dados+"linhas");
-		} catch (Exception e) {
-			System.out.println("Error: " + e.getMessage());
-		}
+	
 	}
 }
