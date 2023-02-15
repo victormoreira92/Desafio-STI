f49f7746de0b5d520a927da81b10ee2e6ee1c68d
victormoreira92 <victormoreira.ti@outlook.com>
2023-02-15 14:57:27 -0300

    criacao de rascunhos de testes para calculo de CR

---
+++ Desafio3/src/com/victormoreira/test/CalculoCRTest.java
@@ -0,0 +1,18 @@
+package com.victormoreira.test;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class CalculoCRTest {
+	
+	@Test
+	public void deveriaAluno100TerCR7() {
+		Universidade universidade = new Universidade();
+		Aluno aluno = universidade.getAlunoPelaMatricula(100);
+		double cr = CoeficienteRendimento.calcular(Aluno);
+		
+	    assertEquals("Valor de CR incorreto.", 7.5, cr, 0);
+		
+	}
+}
