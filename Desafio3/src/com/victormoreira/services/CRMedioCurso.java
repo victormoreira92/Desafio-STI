08c5d5bee0751f357b5ec37afe226fcf8677b5ce
victormoreira92 <victormoreira.ti@outlook.com>
2023-02-16 20:06:42 -0300

    add funcao calcular CR medio

--- Desafio3/src/com/victormoreira/services/CRMedioCurso.java
+++ Desafio3/src/com/victormoreira/services/CRMedioCurso.java
@@ -1,11 +1,19 @@
 package com.victormoreira.services;
 
+import java.util.List;
+
+import com.victormoreira.models.Aluno;
 import com.victormoreira.models.Curso;
 
 public class CRMedioCurso {
 
 	public static double calcular(Curso curso) {
-		return ;
+		double crAlunos = 0;
+		List<Aluno> alunosCurso = curso.getAlunosCursantes();
+		for(Aluno aluno: alunosCurso) {
+			crAlunos = CoeficienteRendimento.calcular(aluno);
+		}
+		return crAlunos / curso.getAlunosCursantes().size() ;
 	}
 
 }
