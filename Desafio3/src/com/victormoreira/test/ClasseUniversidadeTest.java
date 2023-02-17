bb1847483e6698736501a26af38aaaeabcba2ab9
victormoreira92 <victormoreira.ti@outlook.com>
2023-02-17 14:06:16 -0300

    add teste para observar a quantidade de aluno,cursos e Disciplinas

--- Desafio3/src/com/victormoreira/test/ClasseUniversidadeTest.java
+++ Desafio3/src/com/victormoreira/test/ClasseUniversidadeTest.java
@@ -13,12 +13,47 @@ public class ClasseUniversidadeTest {
 	
 	private Universidade universidade = CarregarDadosCSV.carregarDados();
 
+	
+	@Test
+	public void numeroDeCursosNaUniversidadeDeveSer5() {
+		int numeroDisciplinas = universidade.getCursosOferecidos().size();
+		
+		assertEquals(5, numeroDisciplinas);
+		
+	}
+	
+	@Test
+	public void numeroDeAlunosDaUniversidadeDeveriaSer17() {
+		int numeroAlunosMatriculados = universidade.getListaAlunos().size();
+		
+		assertEquals(17, numeroAlunosMatriculados);
+		
+	}
+	
+	@Test
+	public void numeroDeDisciplinasNoCurso4DeveriaSer86() {
+		int numeroDisciplinas = universidade.getCursoPorCodigo("4").getDisciplinasCurso().size();
+		
+		assertEquals(86, numeroDisciplinas);
+		
+	}
+	
+	
 	@Test
 	public void curso21DeveRetornar11Aluno() {
 		Curso curso = universidade.getCursoPorCodigo("21");
 		int numeroAlunos = curso.getAlunosCursantes().size();
 		
 		assertEquals(11, numeroAlunos);
+		
+	}
+	
+	@Test
+	public void curso21Retornar12Disciplinas() {
+		Curso curso = universidade.getCursoPorCodigo("21");
+		int numeroDisciplina = curso.getDisciplinasCurso().size();
+		
+		assertEquals(12, numeroDisciplina);
 		
 	}
 	
