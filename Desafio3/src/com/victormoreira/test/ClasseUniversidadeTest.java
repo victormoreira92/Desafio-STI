fed81fa8392d96aab0b7d1f51370d91831c43e06
victormoreira92 <victormoreira.ti@outlook.com>
2023-02-17 12:44:11 -0300

    add teste para curso 21 retornar 11 alunos

--- Desafio3/src/com/victormoreira/test/ClasseUniversidadeTest.java
+++ Desafio3/src/com/victormoreira/test/ClasseUniversidadeTest.java
@@ -1,5 +1,25 @@
 package com.victormoreira.test;
 
-public class ClasseUniversidadeTest {
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.Test;
+
+import com.victormoreira.models.Curso;
+import com.victormoreira.models.Universidade;
+import com.victormoreira.services.CRMedioCurso;
+import com.victormoreira.services.CarregarDadosCSV;
+
+public class ClasseUniversidadeTest {
+	
+	private Universidade universidade = CarregarDadosCSV.carregarDados();
+
+	@Test
+	public void curso21DeveRetornar11Aluno() {
+		Curso curso = universidade.getCursoPorCodigo("21");
+		int numeroAlunos = curso.getAlunosCursantes().size();
+		
+		assertEquals(11, numeroAlunos);
+		
+	}
+	
 }
